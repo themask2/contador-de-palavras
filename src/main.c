cbd7464851dfb8c38b23477e9cc96c92613e7d91
themask2 <deny.stanley.06@gmail.com>
2017-08-05 01:28:54 -0300

    programa funcionando para todos os casos inclusive ponto flutuante

--- src/main.c
+++ src/main.c
@@ -6,17 +6,49 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
 
-  char c;
+ char c = '1', ant1, ant2; //caracter de armazenamento, anterior 1 e anterior 2
+ int n = 0, flag = 0;// numero de palavras, flag que indica o fim da contagem por palavra
 
-  c = 1;
+ while (c != '\n') {
+ 	scanf("%c", &c);
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
+ 	if (c == ' ') {
+ 		flag = 0;
+ 	}
+ 	if (c == '-') {
+ 		flag = 0;
+ 	}
+ 	if (c == ',') { 
+ 		flag = 0;
+ 	}
+ 	if (c == '.') { 
+ 		flag = 0;		
+ 	}
+	if ( ((ant2 >= '0') && (ant2 <= '9')) && (ant1 == ',') && ((c >= '0') && (c <= '9')) ) { //numero,numero caso especial onde temos um ponto flutuante
+		n--;
+	}
+	if ( ((ant2 >= '0') && (ant2 <= '9')) && (ant1 == '.') && ((c >= '0') && (c <= '9')) ) { //numero.numero caso especial onde temos um ponto flutuante
+		n--;
+	}
 
-  printf("1\n");
-  return 0;
+
+ 	if ( ( ((c >= '0') && (c <= '9')) || ((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')) ) && (!flag) ) {
+ 		n++;
+ 		flag = 1;
+	}
+	
+ 	ant2 = ant1;
+ 	ant1 = c;
+
+ }
+
+
+
+ printf("%d\n", n);
+ return 0;
 }
